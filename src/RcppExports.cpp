fe67eca347b459ddf961c6651d31f894f655535e
Matt P. Wand <matt.wand@uts.edu.au>
2023-03-31 05:43:36 +0000

    version 1.0-2.2

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,6 +7,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // PoiSMMsliceInner
 List PoiSMMsliceInner(int numMCMC, int ncX, arma::vec y, arma::mat Cmat, arma::vec CTy, double sigmabetaHYP, double AHYP, int msgCode);
 RcppExport SEXP _densEstBayes_PoiSMMsliceInner(SEXP numMCMCSEXP, SEXP ncXSEXP, SEXP ySEXP, SEXP CmatSEXP, SEXP CTySEXP, SEXP sigmabetaHYPSEXP, SEXP AHYPSEXP, SEXP msgCodeSEXP) {
--- src/stanExports_PoissonSimpleMixedModel.cc
+++ src/stanExports_PoissonSimpleMixedModel.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4PoissonSimpleMixedModel_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_PoissonSimpleMixedModel")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_PoissonSimpleMixedModel")
 
     .constructor<SEXP,SEXP,SEXP>()
 
